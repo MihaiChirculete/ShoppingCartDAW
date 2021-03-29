b23b6a3e5f95498c4055bf860081ad9e6c89cf24
Chalapa13 <mihai.chirculete98@gmail.com>
2021-03-29 14:29:36 +0300

    Add project files.

---
+++ Models/Author.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShoppingCart.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Biography { get; set; }
+        
+        [NotMapped]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + ' ' + LastName;
+            }
+        }
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
