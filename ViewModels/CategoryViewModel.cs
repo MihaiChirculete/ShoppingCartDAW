12a2910f3775c54e061c0972c924186ba5ee70e2
Chalapa13 <mihai.chirculete98@gmail.com>
2021-03-29 16:19:18 +0300

    Adaugat CategoryViewModel

---
+++ ViewModels/CategoryViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace ShoppingCart.ViewModels
+{
+    /**
+     * Au fost scoase anumite campuri din Category model pentru a nu include si lista de carti
+     * intrucat view-ul ce utilizeaza acest model nu are nevoie sa afiseze cartile
+     */
+    public class CategoryViewModel
+    {
+        [JsonProperty(PropertyName = "id")]
+        public int Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+    }
+}
