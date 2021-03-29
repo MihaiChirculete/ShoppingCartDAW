051f6b99a37398ed6bfe0db5e505e155d4ae78ac
Chalapa13 <mihai.chirculete98@gmail.com>
2021-03-29 16:25:05 +0300

    Adaugat view model pentru Cart

---
+++ ViewModels/CartViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace ShoppingCart.ViewModels
+{
+    /**
+     * Nu am inclus si SessionId deoarece acesta este identificatorul unic al serverului
+     * si nu trebuie expus publicului
+     */
+    public class CartViewModel
+    {
+        [JsonProperty(PropertyName = "id")]
+        public int Id { get; set; }
+
+        [JsonProperty(PropertyName = "cartItems")]
+        public virtual ICollection<CartItemViewModel> CartItems { get; set; }
+    }
+}
